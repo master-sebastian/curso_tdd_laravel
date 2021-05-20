989049d65acf4bd3caf381ad13f5e7a34547aba1
Sebastian  Gutierrez <orjuela.desarrollador@gmail.com>
2021-05-19 22:31:29 -0500

    refactorizando las validaciones

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Repository;
 use Illuminate\Http\Request;
+use App\Http\Requests\RepositoryRequest;
 /**
  *  Este controlador tiene las funcionalidad respetivas para efectuar un crud completo
  */
@@ -20,15 +21,11 @@ class RepositoryController extends Controller
     
     /** 
      * Crear un nuevo repositorio considerando el usuario que se invocucra en el request
-     * @param Request $request Objeto request
+     * @param RepositoryRequest $request Objeto request
      * @return redirect Rediecciona a una ruta especificada dentro de este metodo
      */
-    public function store(Request $request)
+    public function store(RepositoryRequest $request)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required',
-        ]);
         $request->user()->repositories()->create($request->all());
 
         return redirect()->route('repositories.index');
@@ -52,16 +49,12 @@ class RepositoryController extends Controller
     /**
      * Permite modificar la informacion de forma completa del repositorio considerando que le debe pertenecer al usuario
      * para efectuar su respectiva modificacion
-     * @param Request $request objeto request
+     * @param RepositoryRequest $request objeto request
      * @param Repository $repository objeto del modelo repositorio
      * @return redirect Redirecciona a una ruta especificada dentro de este metodo
      */
-    public function update(Request $request, Repository $repository)
+    public function update(RepositoryRequest $request, Repository $repository)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required',
-        ]);
         if ($request->user()->id != $repository->user_id) {
             abort(403);
         }
